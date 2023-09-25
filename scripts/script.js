1c7943baa9bfc60147901da7fd965c258a3ec987
shoproizoshlo <iamsusunny@gmail.com>
2023-09-25 18:57:06 +0200

    fixed navigation menu

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,5 @@ document.querySelector(".header_burger").addEventListener("click", (event) => {
   document.querySelector(".header_burger").classList.toggle("active");
   document.querySelector(".header_menu").classList.toggle("active");
   document.querySelector("body").classList.toggle("lock");
+  document.querySelector("body").classList.toggle("overlay");
 });
