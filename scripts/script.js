79903cc1f053f3c50d36e753add42af5477ab828
shoproizoshlo <iamsusunny@gmail.com>
2023-10-02 11:56:55 +0200

    fixed issue with overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-document.querySelector(".header_burger").addEventListener("click", (event) => {
-  document.querySelector(".header_burger").classList.toggle("active");
-  document.querySelector(".header_menu").classList.toggle("active");
-  document.querySelector("body").classList.toggle("lock");
-  document.querySelector("body").classList.toggle("overlay");
-});
+// document.querySelector(".header_burger").addEventListener("click", (event) => {
+//   document.querySelector(".header_burger").classList.toggle("active");
+//   document.querySelector(".header_menu").classList.toggle("active");
+//   document.querySelector("body").classList.toggle("lock");
+//   document.querySelector("body").classList.toggle("overlay");
+//   document.getElementsByTagName("main")[0].classList.toggle("hide");
+// });
+
+
